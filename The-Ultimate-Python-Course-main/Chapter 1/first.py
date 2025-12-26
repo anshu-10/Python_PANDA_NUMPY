5dc553596e4f7d63d9227b8e8f72761640d405fe
Anshu <anshujamare10@gmail.com>
2025-12-27 00:57:45 +0530

    Initial commit

---
+++ The-Ultimate-Python-Course-main/Chapter 1/first.py
@@ -0,0 +1,5 @@
+# https://www.codewithharry.com/tutorial/python-modules?gad_source=2&gad_campaignid=22773699355&gclid=CjwKCAjwtfvEBhAmEiwA-DsKjkmqn7aeB1tdZqZ1tcRe-rWIDhL3af91jwP2zjLvqwZrLiX8p_p1YBoCZO4QAvD_BwE
+
+# tutorial code notes
+
+print("Hello World")
